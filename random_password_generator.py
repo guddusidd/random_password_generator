374436bf791adc8a038be1d3325fbc42f01af707
siddht4 <15411956+siddht4@users.noreply.github.com>
2022-07-31 10:54:53 +0530

    Function,new method
    Made to function
    Used random range 
    Added seed

--- random_password_generator.py
+++ random_password_generator.py
@@ -1,13 +1,15 @@
 import random
-_pwd=''
-for _ in range(0,100):
-    #print(chr(random.randint(0,130)))
-    #print(chr(_))
-    random.seed()
-    random.getstate()
-    _c=chr(random.randint(33,126))
-    _pwd+=_c
-print(_pwd)
+def _rand_pass(length=100):
+    _pwd=''
+    for _ in range(0,length):
+
+        random.seed()
+        random.getstate()
+        _c=chr(random.randrange(33,126))
+        _pwd+=_c
+    return _pwd
+_ab=_rand_pass()
+print(_ab)
 with open('_pass.txt','w') as _fw:
-    _fw.write(_pwd)
+    _fw.write(_ab)
     
